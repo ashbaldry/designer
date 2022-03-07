94fd4a3782a390520ebc170a12ff0e1de795ba17
Ashley Baldry <arbaldry91@gmail.com>
2022-03-07 20:31:18 +0000

    Adding more tests

--- tests/testthat/test-json-to-r.R
+++ tests/testthat/test-json-to-r.R
@@ -1,4 +1,17 @@
-testthat::test_that("Empty list returns nothing", {
-  testthat::expect_equal(htmlToRScript(NULL), "")
-  testthat::expect_equal(htmlToRScript(list()), "")
+testthat::test_that("NULL returns empty string", {
+  testthat::expect_equal(jsonToRScript(NULL), "")
+})
+
+testthat::test_that("Invalid JSON returns NA", {
+  testthat::expect_message(jsonToRScript(""))
+  testthat::expect_equal(suppressMessages(jsonToRScript("")), NA_character_)
+})
+
+testthat::test_that("Empty JSON object returns empty string", {
+  testthat::expect_equal(jsonToRScript("{}"), "")
+})
+
+testthat::test_that("JSON list returns valid R code", {
+  ui_script <- jsonToRScript('{"r_function": "fluidPage"}')
+  testthat::expect_true(grepl("fluidPage", ui_script))
 })
