97b1dee741f16d619db04d602ede7077ff8d7dc1
Ashley Baldry <arbaldry91@gmail.com>
2022-03-07 20:56:33 +0000

    Separating out JS functions

--- inst/app/www/designer-app-utils.js
+++ inst/app/www/designer-app-utils.js
@@ -1,29 +1,3 @@
-$(document).ready(function() {
-  $("#sidebar-page_type").on("change", updateCanvasCheck);
-
-  $(".page-canvas").html(createCanvasPage($("#sidebar-page_type").val()));
-  enableSortablePage(document.getElementById("canvas-page"));
-
-  $("#confirm_reset").on("click", () => {
-    $(".page-canvas").html(createCanvasPage($("#sidebar-page_type").val()));
-    enableSortablePage(document.getElementById("canvas-page"));
-  });
-
-  $(".copy-ui-button").on("click", copyUICode);
-
-  Sortable.create(document.getElementById("sidebar-bin"), {
-    group: {
-      name: "shared",
-      pull: false
-    },
-    handle: ".designer-element",
-    draggable: ".designer-element",
-    onAdd: function (evt) {
-      this.el.removeChild(evt.item);
-    }
-  });
-});
-
 enableSortablePage = function(el) {
   Sortable.create(el, {
     group: {
