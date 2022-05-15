3f3a3f9f81bc522ec09f69121b40475e678e5303
Ashley Baldry <arbaldry91@gmail.com>
2022-05-15 19:37:49 +0100

    Adding first update of vignette

--- R/designer.R
+++ R/designer.R
@@ -1,5 +1,8 @@
 #' @description
 #' To learn more about \code{designer}, have a read of the vignette: \code{vignette("designer")}
 #'
+#' @import shiny
+#' @importFrom stats setNames
+#'
 #' @keywords internal
 "_PACKAGE"
