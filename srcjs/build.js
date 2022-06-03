3bf4be6d51273a7c241ba6ecfac14ca6a0160b29
Ashley Baldry <arbaldry91@gmail.com>
2022-06-03 18:28:07 +0100

    Moving input into module

--- srcjs/build.js
+++ srcjs/build.js
@@ -1,12 +1,9 @@
-const minify = require('@node-minify/core');
-const uglifyJS = require('@node-minify/uglify-js');
-
 require("esbuild").build({
   entryPoints: ['input/canvas-page-input.js'],
   bundle: true,
-  platform: "node",
-  external: ["./node_modules/*"],
-  outdir: "tmp"
+  external: ['./node_modules/*', 'rstudio-shiny'],
+  outdir: 'tmp',
+  platform: 'node'
 }).catch(
   () => process.exit(1)
 );
--- srcjs/index.js
+++
@@ -1,7 +0,0 @@
-require("esbuild").build({
-  entryPoints: ["input/canvas-page-input.js"],
-  bundle: true,
-  outfile: "tmp"
-}).catch(
-  () => process.exit(1)
-);
