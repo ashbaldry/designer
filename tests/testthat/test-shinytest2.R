a6d911176607828f1edac398e98af0cd7fb19909
Ashley Baldry <arbaldry91@gmail.com>
2022-06-12 16:54:20 +0100

    Adding extra tests to make sure generation happens

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -26,8 +26,15 @@ testthat::test_that("designer app works", {
   ui <- app$get_value(input = "canvas-canvas")
   testthat::expect_true(grepl("fluidPage(", jsonToRScript(ui), fixed = TRUE))
 
+  # Expect all components create a component that can be dragged
+  shiny_components <- setdiff(COMPONENTS, c(BS4_COMPONENTS, NAVBAR_COMPONENTS))
+  for (component in shiny_components) {
+    app$click(selector = paste0("#settings-component .component-item[name='", component, "']"))
+    testthat::expect_true(app$get_html("#sidebar-container") != "")
+  }
+
   # Choose all different outputs that create IDs
-  app$click(selector = ".component-item[name='output']")
+  app$click(selector = "#settings-component .component-item[name='output']")
   original_outputs <- app$get_values()$output
   app$set_inputs("sidebar-type" = "plot")
   app$set_inputs("sidebar-type" = "table")
