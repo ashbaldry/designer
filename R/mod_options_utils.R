5274a649dffced560581908620e808aa4835e1d3
Ashley Baldry <arbaldry91@gmail.com>
2022-12-18 12:50:22 +0000

    Fixing error appearing on JS console

--- R/mod_options_utils.R
+++ R/mod_options_utils.R
@@ -4,7 +4,7 @@ CSSFileInput <- function(id, label) {
     tags$label(label),
     div(
       class = "custom-file",
-      tags$input(id = id, type = "file", class = "custom-file-input", accept = ".css"),
+      tags$input(id = id, type = "file", accept = ".css"),
       tags$label(class = "custom-file-label", `for` = id, "Choose file")
     )
   )
